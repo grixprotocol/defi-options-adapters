19546a0d83e739addb445ef98b3affafe5f44c84
Jack Alwyn <idan.4tal@gmail.com>
2024-07-04 16:20:32 +0300

    refactor: update pseudo code interfaces for DeFi options adapters in TypeScript

---
+++ pseudo_code_interfaces.ts
@@ -0,0 +1,108 @@
+
+// Pseudo code interfaces for DeFi options adapters in TypeScript
+
+function fetchExpiryDates(): string[] {
+    /**
+     * Fetch the expiry dates for options available.
+     * 
+     * Returns:
+     * string[]: A list of expiry dates.
+     */
+    return [];
+}
+
+function listStrikePricesByExpiry(expiryDate: string): number[] {
+    /**
+     * List the strike prices for options available for a given expiry date.
+     * 
+     * Parameters:
+     * expiryDate (string): The expiry date for which to list strike prices.
+     * 
+     * Returns:
+     * number[]: A list of strike prices.
+     */
+    return [];
+}
+
+function getLongOptionPremium(strikePrice: number, expiryDate: string): number {
+    /**
+     * Get the premium for a long option for a given strike price and expiry date.
+     * 
+     * Parameters:
+     * strikePrice (number): The strike price of the option.
+     * expiryDate (string): The expiry date of the option.
+     * 
+     * Returns:
+     * number: The premium for the long option.
+     */
+    return 0;
+}
+
+function buyOptionContract(strikePrice: number, expiryDate: string, amount: number): object {
+    /**
+     * Buy an option contract for a given strike price, expiry date, and amount.
+     * 
+     * Parameters:
+     * strikePrice (number): The strike price of the option.
+     * expiryDate (string): The expiry date of the option.
+     * amount (number): The amount of the option to buy.
+     * 
+     * Returns:
+     * object: An object containing the details of the purchased option contract.
+     */
+    return {};
+}
+
+function addOptionToPosition(optionContract: object): object {
+    /**
+     * Add an option contract to an existing position.
+     * 
+     * Parameters:
+     * optionContract (object): The option contract to add to the position.
+     * 
+     * Returns:
+     * object: An object containing the updated position details.
+     */
+    return {};
+}
+
+function exerciseOptionContract(optionContract: object): object {
+    /**
+     * Exercise an option contract.
+     * 
+     * Parameters:
+     * optionContract (object): The option contract to exercise.
+     * 
+     * Returns:
+     * object: An object containing the details of the exercised option contract.
+     */
+    return {};
+}
+
+function sellOptionBackToIssuer(optionContract: object): object {
+    /**
+     * Sell an option contract back to the issuer.
+     * 
+     * Parameters:
+     * optionContract (object): The option contract to sell back to the issuer.
+     * 
+     * Returns:
+     * object: An object containing the details of the sold option contract.
+     */
+    return {};
+}
+
+function transferOptionOwnership(optionContract: object, newOwner: string): object {
+    /**
+     * Transfer the ownership of an option contract to a new owner.
+     * 
+     * Parameters:
+     * optionContract (object): The option contract to transfer.
+     * newOwner (string): The new owner of the option contract.
+     * 
+     * Returns:
+     * object: An object containing the details of the transferred option contract.
+     */
+    return {};
+}
+
