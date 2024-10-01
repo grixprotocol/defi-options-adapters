3d6cff819ef4297bc29fc68f1874bcb85158e2cc
Tomelia1999 <tom.elia99@gmail.com>
2024-10-01 11:20:19 +0300

    adding getPremium snippets for Short and Long positions

---
+++ zomma/getShortOptionsPremium.ts
@@ -0,0 +1,37 @@
+import Zomma from "zomma.js";
+import * as dotenv from "dotenv";
+import { ethers } from "ethers";
+dotenv.config();
+
+const init = async () => {
+  const zomma = new Zomma({
+    privateKey: process.env.PRIVATE_KEY,
+    network: "mainnet", // or 'testnet' for the test network
+  });
+  await zomma.initialize();
+
+  const markets = await zomma.getMarkets();
+  const buySize = ethers.parseEther(`0.1`);
+  const strike = ethers.parseEther(`60000`);
+
+  const buyCallBTC = async () => {
+    // BTC Buy Call Expiry 2024/9/6 60,000  0.1 size
+    const quote = await zomma.getPremium(
+      markets[0], //BTC-USDC
+      1725609600,
+      strike,
+      true,
+      buySize.toString()
+    );
+    console.log(quote);
+    const premium = quote[0];
+    const fee = quote[1];
+
+    console.log(ethers.formatEther(premium));
+    console.log(ethers.formatEther(fee));
+  };
+
+  await buyCallBTC();
+};
+
+init();
